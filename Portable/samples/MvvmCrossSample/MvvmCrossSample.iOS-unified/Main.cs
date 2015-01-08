589b8e66e2f5e514b975b9009df5fb67b76e0cda
Matthew Leibowitz <mattleibow@live.com>
2015-01-09 01:42:24 +0200

    Added a unified sample project

---
+++ Portable/samples/MvvmCrossSample/MvvmCrossSample.iOS-unified/Main.cs
@@ -0,0 +1,12 @@
+﻿using UIKit;
+
+namespace MvvmCrossSample.iOS
+{
+	public class Application
+	{
+		public static void Main(string[] args)
+		{
+			UIApplication.Main(args, null, typeof(AppDelegate));
+		}
+	}
+}
---
+++ Portable/samples/MvvmCrossSample/MvvmCrossSample.iOS-unified/Views/BaseView.cs
@@ -0,0 +1,14 @@
+﻿using Cirrious.MvvmCross.Dialog.Touch;
+using UIKit;
+
+namespace MvvmCrossSample.iOS.Views
+{
+	public abstract class BaseView : MvxDialogViewController
+	{
+		protected BaseView()
+			: base (UITableViewStyle.Grouped, null, true)
+		{
+			EdgesForExtendedLayout = UIRectEdge.None;
+		}
+	}
+}
